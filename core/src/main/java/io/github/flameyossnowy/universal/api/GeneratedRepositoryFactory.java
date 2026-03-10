f36c076057217ad0881d4bc78f253aa42ee16297
flameyosflow <flamesfflowsnow@gmail.com>
2026-03-10 15:21:30 +0200

    Add GeneratedRepositoryFactory

---
+++ core/src/main/java/io/github/flameyossnowy/universal/api/GeneratedRepositoryFactory.java
@@ -0,0 +1,9 @@
+package io.github.flameyossnowy.universal.api;
+
+/**
+ * Marker interface implemented by all generated repository factory classes.
+ * Discovered at runtime via {@link java.util.ServiceLoader}.
+ */
+public interface GeneratedRepositoryFactory {
+    // no methods needed - presence alone triggers class loading
+}
